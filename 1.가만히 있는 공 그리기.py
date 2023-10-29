9b72ae02ec8cbe532b169e3c3ec4c4816f082020
doyoon <dyk6208@gmail.com>
2023-10-29 17:04:08 +0900

    주석 추가

--- 1.가만히 있는 공 그리기.py
+++ 1.가만히 있는 공 그리기.py
@@ -1,17 +1,17 @@
 import pygame
 
-pygame.init()
+pygame.init() # pygame 모듈 사용 시작
 
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width,screen_height))
 
-WHITE = (255,255,255)
+WHITE = (255,255,255) # R, G, B 색상
 
-ball_size = 20
-ball_pos = [screen_width//2, screen_height//2]
-ball_speed = [0,0]
+ball_size = 20 # 반지름
+ball_pos = [ball_size, screen_height//2] # 공 위치
+ball_speed = [0,0] # 공속도, 각각 x축 y축 속도
 
 while 1:
-    pygame.draw.circle(screen, WHITE, ball_pos, ball_size)
-    pygame.display.flip()
+    pygame.draw.circle(screen, WHITE, ball_pos, ball_size) # 어디에 그릴지, 색깔, 공 중심 위치, 공 크기
+    pygame.display.flip() # 화면 전환하기
---
+++ 4.공 위아래로 움직이게 하기.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+FPS = 60
+clock = pygame.time.Clock()
+
+ball_size = 20
+ball_pos = [screen_width//2, screen_height//2]
+ball_speed = [0,2] # 각각 x축, y 축 초기 속도
+move_speed = 4
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        if ball_pos[0]>0: # 화면 왼쪽 끝보다 오른쪽에 있을때
+            ball_pos[0] -= move_speed
+    if keys[pygame.K_RIGHT]:
+        if ball_pos[0] < screen_width:
+            ball_pos[0] += move_speed
+    ball_pos[1] += ball_speed[1] # 아래로 이동
+
+    screen.fill(BLACK)
+    pygame.draw.circle(screen, WHITE, ball_pos, ball_size)
+    pygame.display.flip()
+    clock.tick(FPS)
